d1dc865837e1ad2ce6eab946139b650954139896
drakuna <drakuna@users.noreply.github.com>
2015-05-23 23:53:16 +0700

    create ContactMain.cpp
    draft ideal

---
+++ ContactMain.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <Windows.h>
+#include <time.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to Contact Manager Software" << endl;
+	Sleep(10000);
+	//int i;
+	// Begin(); lua chon hanh dong
+	/* switch (i)
+	{
+	case 1: ThemDanhBa();
+		break;
+	case 2: ...;
+		break;
+	default: Begin();
+		break;
+	} */
+	return 0;
+}
+
+// cac ham chuc nang
+/*ham lua chon hanh dong
+void Begin()
+1. se co cac hanh dong nhu 1. Them danh ba; 2. Xoa danh ba; ...
+2. dung switch de chuyen den ham xu ly hanh dong tuong ung
+check gia tri cua i, neu nam ngoai cac lua chon thi yeu cau nhap lai
+*/
+/*void NhapDanhBa()
+1. co can gioi han so ky tu cua ten danh ba khong? neu co gioi han thi can check do dai cua chuoi ky tu nhap vao
+neu qua dai thi yeu cau nhap lai
+2. ten chi bao gom cac chu cai va so, neu co ky tu dac biet thi yeu cau nhap lai
+3. neu ten danh ba da ton tai thi dua ra lua chon, add them thong tin vao danh ba co san, hoac tao 1 danh ba moi
+*/
+/*
+co store danh ba ra file luu o hdd khong, neu co them cac ham xu ly vao
+*/
+/*
+Danh ba luu theo kieu struct(), voi tam thoi la 2 bien kieu string la Ten, va Mobile, sau nay se phat trien them
+*/
+/*
+cac option khac
+*/
